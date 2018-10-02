13a54fbfc249d1b96937ca7a0fb96c37d9f08b42
Philip Jägenstedt <philip@foolip.org>
2018-10-02 15:28:20 +0200

    less ls

--- cirrus.sh
+++ cirrus.sh
@@ -2,4 +2,4 @@
 
 sudo hdiutil attach SafariTechnologyPreview.dmg
 sudo installer -pkg "/Volumes/Safari Technology Preview/Safari Technology Preview.pkg" -target /
-usr/libexec/PlistBuddy -c "print :CFBundleShortVersionString" "/Applications/Safari Technology Preview.app/Contents/Info.plist"
+/usr/libexec/PlistBuddy -c "print :CFBundleShortVersionString" "/Applications/Safari Technology Preview.app/Contents/Info.plist"
