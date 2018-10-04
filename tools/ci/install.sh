2897b2b7ec3b7a8edd4cb81e6ea1a279e8f28a82
Philip Jägenstedt <philip@foolip.org>
2018-10-04 02:08:57 +0200

    Update install.sh

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -ex
 
-if [[ $RUN_JOB -eq 1 ]]; then
-    pip install -U setuptools
-    pip install -U requests
-    pip install -U virtualenv
-fi
+pip install -U setuptools
+pip install -U requests
+pip install -U virtualenv
