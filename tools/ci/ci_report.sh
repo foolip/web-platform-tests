2e4d97c3e2b4672da5908ecfbef255bc200be248
Philip Jägenstedt <philip@foolip.org>
2018-10-09 14:50:45 +0200

    tinker ci_report.sh

--- tools/ci/ci_report.sh
+++ tools/ci/ci_report.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "travis_fold:start:report_base64"
 echo "-----BEGIN WPT REPORT-----"
-cat "$1" | gzip -9 | base64 -b 64
+cat "$1" | gzip -9 | base64 -b 64 | sed '/^$/d'
 echo "-----END WPT REPORT-----"
+echo "travis_fold:end:report_base64"
