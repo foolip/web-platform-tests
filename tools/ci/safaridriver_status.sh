e3f1983329e68218b4d43d3dbb70973d741ba260
Philip Jägenstedt <philip@foolip.org>
2018-10-21 00:43:08 +0200

    Debug safaridriver

--- tools/ci/safaridriver_status.sh
+++ tools/ci/safaridriver_status.sh
@@ -4,14 +4,15 @@ PORT=55555
 "/Applications/Safari Technology Preview.app/Contents/MacOS/safaridriver" -p $PORT &
 PID=$!
 # safaridriver isn't immediately responsive, so loop
+TMPFILE=$(mktemp)
 while true; do
     # https://superuser.com/a/442395
-    http_code=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:$PORT/status")
+    http_code=$(curl -s -S -o $TMPFILE -w "%{http_code}" "http://localhost:$PORT/status")
     if [ "$http_code" == "200" ]; then
         echo "safaridriver status OK"
         break
     fi
-    echo "waiting for safaridriver to start"
+    cat $TMPFILE
     sleep 1
 done
 kill $PID
