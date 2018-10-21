111bcbe24dc6d650852635185eeef7a7fead6485
Philip Jägenstedt <philip@foolip.org>
2018-10-22 01:29:36 +0200

    Start from https://github.com/foolip/wpt/pull/6 commit 63b0e82

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 set -ex
 
-SCRIPT_DIR=$(dirname $(readlink -f "$0"))
-WPT_ROOT=$(readlink -f $SCRIPT_DIR/../..)
+WPT_ROOT=$(cd $(dirname "$0")/../.. && pwd -P)
 cd $WPT_ROOT
 
-if [[ $RUN_JOB -eq 1 ]]; then
-    pip install -U setuptools
-    pip install -U requests
-fi
+which pip || sudo easy_install pip
+
+sudo pip install -U setuptools
+sudo pip install -U requests
+sudo pip install -U virtualenv
+
+HOMEBREW_NO_AUTO_UPDATE=1 brew cask install https://raw.githubusercontent.com/Homebrew/homebrew-cask-versions/23fae0a88868911913c2ee7d527c89164b6d5720/Casks/safari-technology-preview.rb
+# https://web-platform-tests.org/running-tests/safari.html
+sudo "/Applications/Safari Technology Preview.app/Contents/MacOS/safaridriver" --enable
+defaults write com.apple.Safari WebKitJavaScriptCanOpenWindowsAutomatically 1
+# https://github.com/web-platform-tests/results-collection/blob/master/src/scripts/trust-root-ca.sh
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain tools/certs/cacert.pem
+./wpt make-hosts-file | sudo tee -a /etc/hosts
+
+# https://github.com/web-platform-tests/results-collection/issues/218 workaround
+sudo cp fonts/Ahem.ttf /Library/Fonts
