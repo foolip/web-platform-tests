7a1855b085d643771c36657a1e61d87a790cf782
Philip Jägenstedt <philip@foolip.org>
2018-10-30 00:47:47 +0100

    Move install steps into .azure-pipelines.yml

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -5,19 +5,7 @@ SCRIPT_DIR=$(cd $(dirname "$0") && pwd -P)
 WPT_ROOT=$SCRIPT_DIR/../..
 cd $WPT_ROOT
 
-which pip || sudo easy_install pip
-
-sudo pip install -U setuptools
-sudo pip install -U requests
-sudo pip install -U virtualenv
-
-HOMEBREW_NO_AUTO_UPDATE=1 brew cask install Homebrew/homebrew-cask-versions/safari-technology-preview
-# https://web-platform-tests.org/running-tests/safari.html
-sudo "/Applications/Safari Technology Preview.app/Contents/MacOS/safaridriver" --enable
-defaults write com.apple.Safari WebKitJavaScriptCanOpenWindowsAutomatically 1
-# https://github.com/web-platform-tests/results-collection/blob/master/src/scripts/trust-root-ca.sh
-sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain tools/certs/cacert.pem
-./wpt make-hosts-file | sudo tee -a /etc/hosts
-
-# https://github.com/web-platform-tests/results-collection/issues/218 workaround
-sudo cp fonts/Ahem.ttf /Library/Fonts
+if [[ $RUN_JOB -eq 1 ]]; then
+    pip install -U setuptools
+    pip install -U requests
+fi
